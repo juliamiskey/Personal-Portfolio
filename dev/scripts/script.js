c62aec7e16793aed89ea89429a6ac5779d477691
Julia Miskey <juliamiskey@gmail.com>
2018-06-02 12:49:55 -0400

    added main content and  styles to index.html

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -1,38 +1,49 @@
-console.log('connected');
+// SMOOTH SCROLL
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 
-// const gallery = document.querySelector('.gallery');
-// const overlay = document.querySelector('.overlay');
-// const overlayImage = overlay.querySelector('img');
-// const overlayClose = overlay.querySelector('.close');
-// function generateHTML([h, v]) {
-//     return `
-//         <div class="item h${h} v${v}">
-//           <img src="images/${randomNumber(12)}.jpg">
-//           <div class="item__overlay">
-//             <button>View →</button>
-//           </div>
-//         </div>
-//       `;
-// }
+// RETURN TO TOP
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
 
-// function randomNumber(limit) {
-//     return Math.floor(Math.random() * limit) + 1;
-// }
-
-// function handleClick(e) {
-//     const src = e.currentTarget.querySelector('img').src;
-//     overlayImage.src = src;
-//     overlay.classList.add('open');
-// }
-
-// function close() {
-//     overlay.classList.remove('open');
-// }
-
-// const digits = Array.from({ length: 50 }, () => [randomNumber(4), randomNumber(4)]).concat([[1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1], [1, 1]])
-
-// const html = digits.map(generateHTML).join('');
-// gallery.innerHTML = html;
-// const items = document.querySelectorAll('.item');
-// items.forEach(item => item.addEventListener('click', handleClick));
-// overlayClose.addEventListener('click', close);
+function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        document.getElementById("top").style.display = "block";
+    } else {
+        document.getElementById("top").style.display = "none";
+    }
+}
